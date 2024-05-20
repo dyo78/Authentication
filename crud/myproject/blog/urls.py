d5a07f3486d9aac9ec95503887fe330973c29a58
dyo78 <maharjan.diyaa@gmail.com>
2024-05-20 17:52:08 +0545

    Blogs views,url changed

--- crud/myproject/blog/urls.py
+++ crud/myproject/blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import BlogDetail,BlogInfo
+from .views import BlogDetail, BlogInfo
 
-urlpatterns=[
-
+urlpatterns = [
     path("detail/", BlogDetail.as_view(), name='blog'),
-    path("detail/<int:id>/", BlogInfo.as_view()),
+    path("my-blogs/", BlogInfo.as_view(), name='blog-info own'),
 
+    path("my-blogs/<int:pk>/", BlogInfo.as_view(), name='blog-info'),
 ]
--- crud/myproject/blog/views.py
+++ crud/myproject/blog/views.py
@@ -1,117 +1,136 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import View
-from .models import Blog, CustomUser
 from django.http import JsonResponse
-import json
+from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from blog.models import Blog
+from django.shortcuts import get_object_or_404
+from myapp.models import CustomUser
+import jwt
+from functools import wraps
 from django.utils.decorators import method_decorator
+import json
 
-@method_decorator(csrf_exempt, name='dispatch')
-class BlogInfo(View):
-    def get(self, request, id):
-        blog = get_object_or_404(Blog, id=id)
-        blog_data = {
-            'id': blog.id,
-            'title': blog.title,
-            'desc': blog.desc,
-            'author': blog.author.email,  
-            'created_at': blog.created_at,
-            'edited_at': blog.edited_at
-        }
-        return JsonResponse(blog_data, status=200)
+def login_and_permission_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        auth_header = request.headers.get('Authorization')
 
-    def put(self, request, id):
-        blog = get_object_or_404(Blog, id=id)
-        data = json.loads(request.body)
-        blog.title = data.get('title', blog.title)
-        blog.desc = data.get('desc', blog.desc)
-        
-        author_id = data.get('author')
-        if author_id:
-            author = get_object_or_404(CustomUser, id=author_id)
-            blog.author = author
+        if not auth_header:
+            return JsonResponse({'error': 'Authorization header missing'}, status=401)
 
-        blog.created_at = data.get('created_at', blog.created_at)
-        blog.edited_at = data.get('edited_at', blog.edited_at)
-        blog.save()
+        try:
+            token = auth_header.split(' ')[1]
+            access_token_payload = jwt.decode(token, 'secret', algorithms='HS256')
+            request.user_id = access_token_payload['id']
+            request.user = CustomUser.objects.get(pk=request.user_id)
+        except (jwt.exceptions.DecodeError, jwt.exceptions.ExpiredSignatureError, jwt.exceptions.InvalidTokenError):
+            return JsonResponse({'error': 'Invalid or expired token'}, status=401)
+        except CustomUser.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
 
-        blog_data = {
-            'id': blog.id,
-            'title': blog.title,
-            'desc': blog.desc,
-            'author': blog.author.email,  
-            'created_at': blog.created_at,
-            'edited_at': blog.edited_at
-        }
-        return JsonResponse(blog_data, status=200)
+        blog_id = kwargs.get('pk')
+        if blog_id:
+            blog = get_object_or_404(Blog, id=blog_id)
+            if blog.author.id != request.user_id:
+                return JsonResponse({'error': 'Unauthorized access to blog'}, status=403)
 
-    def patch(self, request, id):
-        blog = get_object_or_404(Blog, id=id)
-        data = json.loads(request.body)
-        blog.title = data.get('title', blog.title)
-        blog.desc = data.get('desc', blog.desc)
+        return view_func(request, *args, **kwargs)
 
-        author_id = data.get('author')
-        if author_id:
-            author = get_object_or_404(CustomUser, id=author_id)
-            blog.author = author
+    return _wrapped_view
 
-        blog.created_at = data.get('created_at', blog.created_at)
-        blog.edited_at = data.get('edited_at', blog.edited_at)
-        blog.save()
-
-        blog_data = {
-            'id': blog.id,
-            'title': blog.title,
-            'desc': blog.desc,
-            'author': blog.author.email,  
-            'created_at': blog.created_at,
-            'edited_at': blog.edited_at
-        }
-        return JsonResponse(blog_data, status=200)
-
-    def delete(self, request, id):
-        blog = get_object_or_404(Blog, id=id)
-        blog.delete()
-        return JsonResponse({"msg": "blog removed successfully"}, status=204)
-
-
-@method_decorator(csrf_exempt, name='dispatch')
 class BlogDetail(View):
+    @method_decorator(csrf_exempt)
+    @method_decorator(login_and_permission_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def get(self, request):
-        blogs = Blog.objects.all()
-        blogs_data = []
-        for blog in blogs:
-            blog_data = {
-                'id': blog.id,
-                'title': blog.title,
-                'desc': blog.desc,
-                'author': blog.author.email,  
-                'created_at': blog.created_at,
-                'edited_at': blog.edited_at,
-                'is_deleted': blog.is_deleted
-            }
-            blogs_data.append(blog_data)
-        return JsonResponse(blogs_data, status=200, safe=False)
+        blogs = Blog.objects.filter(is_deleted=False)
+        blog_data = [{'id': blog.id,
+                      'title': blog.title,
+                      'desc': blog.desc,
+                      'created_at': blog.created_at,
+                      'edited_at': blog.edited_at,
+                      'author': blog.author.email if blog.author else 'Unknown'} for blog in blogs]
+        return JsonResponse(blog_data, safe=False)
 
     def post(self, request):
-        data = json.loads(request.body)
-        
-        author_id = data.get('author')
-        author = get_object_or_404(CustomUser, id=author_id)
+        blog_data = json.loads(request.body)
+        try:
+            blog = Blog.objects.create(title=blog_data['title'],
+                                       desc=blog_data['desc'],
+                                       author=request.user)
+            return JsonResponse({'id': blog.id,
+                                 'title': blog.title,
+                                 'desc': blog.desc,
+                                 'created_at': blog.created_at,
+                                 'edited_at': blog.edited_at,
+                                 'author': request.user.email}, status=201)
+        except KeyError:
+            return JsonResponse({'error': 'Invalid data provided'}, status=400)
 
-        blog = Blog.objects.create(
-            title=data['title'],
-            desc=data['desc'],
-            author=author 
-        )
+class BlogInfo(View):
+    @method_decorator(csrf_exempt)
+    @method_decorator(login_and_permission_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
-        blog_data = {
-            'id': blog.id,
-            'title': blog.title,
-            'desc': blog.desc,
-            'author': blog.author.email,  
-            'created_at': blog.created_at,
-            'edited_at': blog.edited_at
-        }
-        return JsonResponse(blog_data, status=201)
+    def get(self, request, pk=None):
+        if pk:
+            blog = get_object_or_404(Blog, id=pk, is_deleted=False)
+            blog_data = {'id': blog.id, 'title': blog.title,
+                         'desc': blog.desc,
+                         'created_at': blog.created_at,
+                         'edited_at': blog.edited_at,
+                         'author': blog.author.email if blog.author else 'Unknown'}
+            return JsonResponse(blog_data)
+        else:
+            blogs = Blog.objects.filter(author=request.user, is_deleted=False)
+            blog_data = [{'id': blog.id,
+                          'title': blog.title,
+                          'desc': blog.desc,
+                          'created_at': blog.created_at,
+                          'edited_at': blog.edited_at,
+                          'author': blog.author.email if blog.author else 'Unknown'} for blog in blogs]
+            return JsonResponse(blog_data, safe=False)
+
+    def put(self, request, pk):
+        blog_data = json.loads(request.body)
+        try:
+            blog = get_object_or_404(Blog, id=pk, author=request.user)
+            blog.title = blog_data['title']
+            blog.desc = blog_data['desc']
+            blog.save()
+            return JsonResponse({'id': blog.id, 'title': blog.title,
+                                 'desc': blog.desc,
+                                 'created_at': blog.created_at,
+                                 'edited_at': blog.edited_at,
+                                 'author': blog.author.email if blog.author else 'Unknown'})
+        except KeyError:
+            return JsonResponse({'error': 'Invalid data provided'}, status=400)
+
+    def patch(self, request, pk):
+        blog_data = json.loads(request.body)
+        try:
+            blog = get_object_or_404(Blog, id=pk, author=request.user)
+            if 'title' in blog_data:
+                blog.title = blog_data['title']
+            if 'desc' in blog_data:
+                blog.desc = blog_data['desc']
+            if 'edited_at' in blog_data:
+                blog.edited_at = blog_data['edited_at']
+            blog.save()
+            return JsonResponse({'id': blog.id, 'title': blog.title,
+                                 'desc': blog.desc,
+                                 'created_at': blog.created_at,
+                                 'edited_at': blog.edited_at,
+                                 'author': blog.author.email if blog.author else 'Unknown'})
+        except KeyError:
+            return JsonResponse({'error': 'Invalid data provided'}, status=400)
+
+    def delete(self, request, pk):
+        try:
+            blog = get_object_or_404(Blog, id=pk, author=request.user)
+            blog.delete()
+            return JsonResponse({'message': 'Blog deleted'}, status=204)
+        except Blog.DoesNotExist:
+            return JsonResponse({'error': 'Blog not found'}, status=404)
